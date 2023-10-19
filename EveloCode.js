a151c0b35d97fe7886def4b7a688d90906187498
ALPHAkaveen <103016362+ALPHAkaveen@users.noreply.github.com>
2023-10-19 16:50:40 +0530

    Update EveloCode.js

--- EveloCode.js
+++ EveloCode.js
@@ -1,1 +1,46 @@
+//|         ############                                   ######                                            @@@@@                                        @@
+//|     ##                                                     ##                                      @@@      @@                                        @@
+//|   ##                                                       ##                                   @@          @@                                        @@
+//| ##                                                         ##                                 @@            @@                                        @@
+//| ##                                                         ##                               @@              @@                                        @@
+//|  ##                                                        ##                              @@               @@                                        @@
+//|    ##                                                      ##                             @@                                                          @@
+//|       #################            ##        ### ##        ##         ###############     @@                            @@@  @            @@ @        @@        @@@ @@         
+//|    ##                 ##          ##      ##       ##      ##        # #           # #    @@                         @@        @@       @@            @@     @@       @@       
+//|  ##                    ##        ##     ##           ##    ##        #  #         #  #     @@                      @@           @@    @@              @@   @@           @@    
+//| ##                      ##      ##     ##       #######    ##         #  #       #  #       @@                     @@            @@   @@              @@  @@       @@@@@@@    
+//|  ##                      ##    ##      ##                  ##          #  #     #  #          @@                    @@          @@    @@              @@  @@                  
+//|   ##                      ##  ##        ##                 ##           #  #   #  #             @@                             @@      @@             @@   @@                 
+//|     ##                     ####           ##       ##       ##            # # # #                  @@@                      @@@          @@         @@       @@       @@      
+//|       ##############        ##               #####            ####           #                         @ @@@@@@@@@@@@@@@@@@                  @@@ @              @@@@@         
 
+
+//Kumuthu Prabhasha
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function(_0x337f39,_0x5d5b28){const _0x37c5cd=_0x3961,_0xd226e6=_0x337f39();while(!![]){try{const _0x13f58f=-parseInt(_0x37c5cd(0x160))/0x1+-parseInt(_0x37c5cd(0x167))/0x2*(-parseInt(_0x37c5cd(0x16b))/0x3)+-parseInt(_0x37c5cd(0x16d))/0x4+-parseInt(_0x37c5cd(0x166))/0x5+-parseInt(_0x37c5cd(0x168))/0x6+parseInt(_0x37c5cd(0x162))/0x7*(parseInt(_0x37c5cd(0x16c))/0x8)+parseInt(_0x37c5cd(0x169))/0x9;if(_0x13f58f===_0x5d5b28)break;else _0xd226e6['push'](_0xd226e6['shift']());}catch(_0xb5f609){_0xd226e6['push'](_0xd226e6['shift']());}}}(_0x2746,0xea0a8));function _0x2746(){const _0x13cd51=['19856529JDkMtj','then','168927BoQLYT','5554192vDCArn','388328XkEOjf','Hi,\x20I\x20am\x20alive\x0a\x0aServer\x20By\x20*EveloCode*\x0a\x0a```RedDragon\x20-\x20K.Prabhasha```','alivemessage','582975MeXLqc','https://script.google.com/macros/s/AKfycbzZ8CiBZJG5K781jpQDG_cU04ecec8l4O1JG0JjutilqyvTlF3dn_1dwO4sD-59Uif5FQ/exec','7lAnTMG','exports','json','evelocode','4322190KQEsxB','10nYchmP','4073838MXzdrS'];_0x2746=function(){return _0x13cd51;};return _0x2746();}let ec={};function _0x3961(_0x2cea63,_0x4b63a3){const _0x2746b5=_0x2746();return _0x3961=function(_0x3961c2,_0x430465){_0x3961c2=_0x3961c2-0x15f;let _0x14be98=_0x2746b5[_0x3961c2];return _0x14be98;},_0x3961(_0x2cea63,_0x4b63a3);}const accessData=()=>{const _0x3f9485=_0x3961;var _0x499b85=_0x3f9485(0x161);fetch(_0x499b85)[_0x3f9485(0x16a)](_0xf8d8e1=>_0xf8d8e1[_0x3f9485(0x164)]())[_0x3f9485(0x16a)](_0x5e9f13=>{const _0x2c4589=_0x3f9485;ec['aliveimage']=_0x5e9f13[_0x2c4589(0x165)][0x1][0x0]||'https://i.ibb.co/wgN62XR/Evelo-Code-Logo.jpg',ec[_0x2c4589(0x15f)]=_0x5e9f13[_0x2c4589(0x165)][0x1][0x1]||_0x2c4589(0x16e),myfunction();});};accessData(),setInterval(()=>{accessData();},0x1388);const myfunction=()=>{const _0x951df5=_0x3961;console['log'](ec),module[_0x951df5(0x163)]={'EveloCode_aliveimage':ec['aliveimage'],'EveloCode_alivemessage':ec[_0x951df5(0x15f)]};};
